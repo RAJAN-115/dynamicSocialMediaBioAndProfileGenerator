a288191b296af5f20f30a4f3fb27c2d16ef820fa
RAJAN-115 <rajanrp115@gmail.com>
2025-09-16 19:03:11 +0530

    JavaScript String Methods, Properties, and Template Literals

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const interestsInput = document.getElementById('interests');
 generateBtn.addEventListener('click', function () {
   const userName = userNameInput.value.trim().toUpperCase();
   const outputBio = document.getElementById('output-bio');
+  let bioText = bioTextInput.value.trim();
 
-  const bioText = bioTextInput.value.trim();
+  if (bioText.trim().includes('developer')) {
+    bioText = '(This user is a verified developer)';
+  }
   // Corrected line below: we get the value from the input and then split it.
-  const interestsArray = interestsInput.value.split(',').map(interest => interest.trim());
-  const interestsList = interestsArray.join('\n');
+  // const interestsArray = interestsInput.value.split(',').map(interest => interest.trim());
+  const interestsList = interestsInput.value
+    .split(',')
+    .map(interest => interest.trim())
+    .join('\n');
 
   if (userName.length <= 3) {
     outputBio.textContent = `Username must be at least 3 characters long.`;
